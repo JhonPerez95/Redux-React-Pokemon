b5269493f977ff7001dce37b3c9e3b6b6b313dbf
jhonperez95 <jhonperez.j95@gmail.com>
2020-06-02 21:11:33 -0500

    Updates

---
+++ precache-manifest.077ce7811e1ae32b4f88841a0e63b513.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b5079a4cfb92064f99d9452b0d4f9e0",
+    "url": "/JhonPerez95/Redux-React-Pokemon/index.html"
+  },
+  {
+    "revision": "425f6af30d912c5fae64",
+    "url": "/JhonPerez95/Redux-React-Pokemon/static/js/2.4347ba40.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/JhonPerez95/Redux-React-Pokemon/static/js/2.4347ba40.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ec0b8714283236446e4",
+    "url": "/JhonPerez95/Redux-React-Pokemon/static/js/main.53a6b6a8.chunk.js"
+  },
+  {
+    "revision": "36c987c87b644b652a86",
+    "url": "/JhonPerez95/Redux-React-Pokemon/static/js/runtime-main.4e9d1c61.js"
+  }
+]);
--- src/components/appBar/appBar.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-import Container from './container';
-
-const AppBar = () => {
-  return <Container />;
-};
-
-export default AppBar;
