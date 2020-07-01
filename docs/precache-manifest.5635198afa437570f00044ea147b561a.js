a6df879c54389b873b2072703b5d5f233a7ecf7d
Jhon Perez <jhonperez.j95@gmail.com>
2020-07-01 13:09:04 -0500

    Build

---
+++ docs/precache-manifest.5635198afa437570f00044ea147b561a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "692301cf5a2c76a5876eff81e6d46808",
+    "url": "/index.html"
+  },
+  {
+    "revision": "425f6af30d912c5fae64",
+    "url": "/static/js/2.4347ba40.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.4347ba40.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1904ebc2788be6faec35",
+    "url": "/static/js/main.797e707f.chunk.js"
+  },
+  {
+    "revision": "06c82afbd358e1e979b1",
+    "url": "/static/js/runtime-main.57c07782.js"
+  }
+]);
